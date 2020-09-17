4eed98ef232705c5a722a56b3427603da006d3b1
Abi <marticiobgl@gmail.com>
2020-09-18 01:47:20 +0800

    chore: add method to get list

--- spell_correction/SpellCorrection.py
+++ spell_correction/SpellCorrection.py
@@ -1,3 +1,11 @@
 class SpellCorrection():
     def __init__(self, data:str):
         self.data = data
+
+    def get_list(self) -> list:
+        data = [line.strip("\n") for line in self.data]
+        lines = [] 
+        for line in data:
+            new_line = line.split(":")
+            lines.append(new_line)
+        return lines
