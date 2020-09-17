81d23d2e104b26e426887a3a607389b1a324dcb6
Abi <marticiobgl@gmail.com>
2020-09-18 02:38:39 +0800

    chore: add sample usage

---
+++ main.py
@@ -0,0 +1,16 @@
+from spell_correction import load_data, SpellCorrection
+
+def main():
+    data = load_data(filename = "spell-errors.txt")
+    data = (data[:50])
+    sc = SpellCorrection(data=data)
+    lines = sc.get_list()
+    corrections = sc.create_dictionary(lines=lines)
+    dictionary = sc.invert_dictionary(corrections=corrections)
+    texts = ["rainning", "raning", "writtings", "forer"]
+    correct_texts = sc.get_batch_correction(words=texts, dictionary=dictionary)
+    for text, correct_text in zip(texts, correct_texts):
+        print(f"{text} => {correct_text}")
+   
+if __name__ == "__main__":
+    main()
