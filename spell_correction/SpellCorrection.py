9ef2a589c8499d27a9d97822e32ec75cafb1ecfa
Abi <marticiobgl@gmail.com>
2020-09-18 01:54:37 +0800

    chore: add method to create dictionary for corrections

--- spell_correction/SpellCorrection.py
+++ spell_correction/SpellCorrection.py
@@ -9,3 +9,21 @@ class SpellCorrection():
             new_line = line.split(":")
             lines.append(new_line)
         return lines
+
+    def create_dictionary(self, lines: list) -> dict:
+        corrections = {}
+        for line in lines:
+            key = line[0]
+            values = line[1]
+            values = values.strip()
+            if "," in values:
+                values = values.split(",")
+                new_values = []
+                for value in values:
+                    value = value.strip()
+                    new_values.append(value)
+                    values = new_values
+                    corrections[key] = values
+            else:
+                corrections[key] = values
+        return corrections
