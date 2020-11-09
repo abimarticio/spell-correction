954643743dc36f0412c0897f5582a8c54f9286b0
Abigail Marticio <marticiobgl@gmail.com>
2020-11-09 08:19:58 +0800

    refactor: reformat SpellCorrection module

--- spell_correction/SpellCorrection.py
+++ spell_correction/SpellCorrection.py
@@ -1,10 +1,10 @@
-class SpellCorrection():
+class SpellCorrection:
     def __init__(self, data: str):
         self.data = data
 
     def get_list(self) -> list:
         data = [line.strip("\n") for line in self.data]
-        lines = [] 
+        lines = []
         for line in data:
             new_line = line.split(":")
             lines.append(new_line)
