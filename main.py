de6da6302ba29c867e34832d9e588073d2b3a336
Abigail Marticio <marticiobgl@gmail.com>
2020-11-09 08:25:19 +0800

    refactor: reformat main module

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from spell_correction import load_data, SpellCorrection
 
+
 def main():
-    data = load_data(filename = "assets/spell-errors.txt")
-    data = (data[:50])
+    data = load_data(filename="assets/spell-errors.txt")
+    data = data[:50]
     sc = SpellCorrection(data=data)
     lines = sc.get_list()
     corrections = sc.create_dictionary(lines=lines)
@@ -11,6 +12,7 @@ def main():
     correct_texts = sc.get_batch_correction(words=texts, dictionary=dictionary)
     for text, correct_text in zip(texts, correct_texts):
         print(f"{text} => {correct_text}")
-   
+
+
 if __name__ == "__main__":
     main()
