c8fd7003cb5febbbb4b2cee43f6f317907b57272
Abigail Marticio <marticiobgl@gmail.com>
2021-01-09 22:06:12 +0800

    docs: add docstring in get_correction function

--- api.py
+++ api.py
@@ -22,12 +22,24 @@ app = Flask(__name__)
 
 @app.route("/spell-correction", methods=["GET"])
 def get_correction():
-  spell_correction = SpellCorrection(data="assets/spell-errors.txt")
-  text = request.args.get("word", default=None)
-  correct_text = spell_correction.get_correction(text)
-  return f"{text} => {correct_text}"
-  
-  
+    """
+    GET request for get_correction.
+
+    Parameters
+    ----------
+    word: str
+        The mispelled input word.
+
+    Returns
+    -------
+    str
+        The correct word.
+    """
+    spell_correction = SpellCorrection(data="assets/spell-errors.txt")
+    text = request.args.get("word", default=None)
+    correct_text = spell_correction.get_correction(text)
+    return f"{text} => {correct_text}"
+
+
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=5000, debug=True)
-  
+    app.run(host="0.0.0.0", port=5000, debug=True)
